a28077c1c8d43aedbdeccb1f5086dc4d0e560f4a
Nick Ford <nickforddesign@gmail.com>
2017-10-16 15:23:52 -0400

    fixing constructor bug

--- src/mixin.js
+++ src/mixin.js
@@ -3,7 +3,7 @@ import { isDef } from './utils'
 
 const makeComputedProp = (vm) => {
   if (!vm.$options.computed) vm.$options.computed = {}
-  vm._collection = vm.$options.collection()
+  vm._collection = vm.$options.collection
   vm.$options.computed.$collection = {
     get() {
       return vm._collection
@@ -13,18 +13,30 @@ const makeComputedProp = (vm) => {
     }
   }
   vm.$options.computed.collection = () => {
-    return vm._collection.models
+    return vm.$collection.models
   }
 }
 
 const init = (vm) => {
+  if (!vm.$options.computed) {
+    vm.$options.computed = {}
+  }
   makeComputedProp(vm)
+}
+
+const init_collections = (vm) => {
+  vm._collection = vm._collection()
 }
 
 export default (Vue) => ({
   beforeCreate() {
     if (isDef(this.$options.collection)) {
       init(this)
+    }
+  },
+  created() {
+    if (isDef(this.$options.collection)) {
+      init_collections(this)
     }
   },
   beforeDestroy() {
